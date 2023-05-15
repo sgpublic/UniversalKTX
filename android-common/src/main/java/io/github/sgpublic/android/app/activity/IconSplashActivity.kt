86050f858a74237eed343e2f179a72d00f3e0788
mhmzx <sgpublic2002@gmail.com>
2023-05-15 13:47:00 +0800

    feature: IconSplashActivity

---
+++ android-common/src/main/java/io/github/sgpublic/android/app/activity/IconSplashActivity.kt
@@ -0,0 +1,16 @@
+package io.github.sgpublic.android.app.activity
+
+import android.annotation.SuppressLint
+import io.github.sgpublic.android.base.app.BaseActivity
+import io.github.sgpublic.android.common.databinding.ActivitySplashBinding
+import io.github.sgpublic.android.core.util.viewBinding
+
+
+/**
+ * @author Madray Haven
+ * @Date 2023/4/12 8:28
+ */
+@SuppressLint("CustomSplashScreen")
+abstract class IconSplashActivity: BaseActivity<ActivitySplashBinding>() {
+    final override val ViewBinding: ActivitySplashBinding by viewBinding()
+}
