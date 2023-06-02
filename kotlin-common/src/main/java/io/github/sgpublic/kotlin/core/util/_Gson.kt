52e6bebe7aa3b3965a9f871ed964e4e3995a6d03
mhmzx <sgpublic2002@gmail.com>
2023-06-02 16:21:01 +0800

    Update _Gson.kt

--- kotlin-common/src/main/java/io/github/sgpublic/kotlin/core/util/_Gson.kt
+++ kotlin-common/src/main/java/io/github/sgpublic/kotlin/core/util/_Gson.kt
@@ -3,11 +3,14 @@ package io.github.sgpublic.kotlin.core.util
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonParseException
 import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
+import java.util.LinkedList
 import kotlin.reflect.KClass
 
 var GSON: Gson = GsonBuilder()
@@ -25,7 +28,7 @@ fun <T: Any> KClass<T>.fromGson(src: String): T {
 }
 
 @Throws(GsonException::class)
-fun <T: Any> KClass<T>.fromGson(src: JsonElement): T {
+fun <T: Any> KClass<T>.fromGson(src: JsonObject): T {
     return try {
         GSON.fromJson(src, this.java)
     } catch (e: JsonSyntaxException) {
@@ -43,14 +46,13 @@ fun <T: Any> Type.fromGson(src: String): T {
 }
 
 @Throws(GsonException::class)
-fun <T: Any> Type.fromGson(src: JsonElement): T {
+fun <T: Any> Type.fromGson(src: JsonObject): T {
     return try {
         GSON.fromJson(src, this)
     } catch (e: JsonSyntaxException) {
         throw GsonException(src, e)
     }
 }
-
 
 fun Any?.toGson(): String {
     return GSON.toJson(this)
