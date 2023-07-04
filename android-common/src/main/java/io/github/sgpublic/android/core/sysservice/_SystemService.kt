2ec7185a476d882ae2cc8348c1a893f205436161
mhmzx <sgpublic2002@gmail.com>
2023-07-04 11:50:23 +0800

    feature: sysInputMethodManager

--- android-common/src/main/java/io/github/sgpublic/android/core/sysservice/_SystemService.kt
+++ android-common/src/main/java/io/github/sgpublic/android/core/sysservice/_SystemService.kt
@@ -3,6 +3,7 @@ package io.github.sgpublic.android.core.sysservice
 import android.app.ActivityManager
 import android.content.Context
 import android.net.ConnectivityManager
+import android.view.inputmethod.InputMethodManager
 
 /**
  * @author Madray Haven
@@ -25,4 +26,9 @@ val Context.sysEthernetManager: EthernetManagerWrapper get() {
         .getField("ETHERNET_SERVICE")
         .get(null) as String
     return EthernetManagerWrapper(getSystemService(name))
+}
+
+val Context.sysInputMethodManager: InputMethodManager get() {
+    return getSystemService(Context.INPUT_METHOD_SERVICE)
+            as InputMethodManager
 }
