b2af1cdaf7334ab4e2e938239ca7abcf56a5bd7c
mhmzx <sgpublic2002@gmail.com>
2023-07-05 13:28:07 +0800

    feature: sysWindowManager

--- android-common/src/main/java/io/github/sgpublic/android/core/sysservice/_SystemService.kt
+++ android-common/src/main/java/io/github/sgpublic/android/core/sysservice/_SystemService.kt
@@ -3,6 +3,7 @@ package io.github.sgpublic.android.core.sysservice
 import android.app.ActivityManager
 import android.content.Context
 import android.net.ConnectivityManager
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 
 /**
@@ -31,4 +32,9 @@ val Context.sysEthernetManager: EthernetManagerWrapper get() {
 val Context.sysInputMethodManager: InputMethodManager get() {
     return getSystemService(Context.INPUT_METHOD_SERVICE)
             as InputMethodManager
+}
+
+val Context.sysWindowManager: WindowManager get() {
+    return getSystemService(Context.WINDOW_SERVICE)
+            as WindowManager
 }
