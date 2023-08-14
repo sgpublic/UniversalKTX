1f928cbdc270467bbb2648d52980b229d92901c8
mhmzx <sgpublic2002@gmail.com>
2023-08-14 11:06:57 +0800

    feature: bitmapResource

---
+++ android-compose/src/main/java/io/github/sgpublic/android/compose/util/_Resource.kt
@@ -0,0 +1,23 @@
+package io.github.sgpublic.android.compose.util
+
+import android.graphics.drawable.BitmapDrawable
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+
+/**
+ * @author Madray Haven
+ * @Date 2023/8/14 11:06
+ */
+
+
+@Composable
+@ReadOnlyComposable
+fun bitmapResource(@DrawableRes id: Int): ImageBitmap {
+    val context = LocalContext.current
+    return (context.getDrawable(id) as BitmapDrawable)
+        .bitmap.asImageBitmap()
+}
