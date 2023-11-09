e899ad1750c81751a510d69e14eae0444add324e
mhmzx <sgpublic2002@gmail.com>
2023-11-09 11:00:02 +0800

    fix: slf4j

--- kotlin-logback/src/main/java/io/github/sgpublic/kotlin/util/_Slf4j.kt
+++ kotlin-logback/src/main/java/io/github/sgpublic/kotlin/util/_Slf4j.kt
@@ -5,12 +5,16 @@ import org.slf4j.LoggerFactory
 
 interface Loggable {
     val Any.log: Logger get() = LoggerFactory.getLogger(
-        if (!this@Loggable::class.isCompanion) javaClass
-        else javaClass.enclosingClass
+        if (!this@Loggable::class.isCompanion)
+            this@Loggable.javaClass
+        else
+            this@Loggable.javaClass.enclosingClass
     )
 }
 
 val Any.log: Logger get() = LoggerFactory.getLogger(
-    if (!this::class.isCompanion) javaClass
-    else javaClass.enclosingClass
+    if (!this::class.isCompanion)
+        javaClass
+    else
+        javaClass.enclosingClass
 )
